57a2979424e4573b2258a45342b92a784b0a27b1
gworthin <102486685+gworthin@users.noreply.github.com>
2022-03-27 16:52:04 -0400

    Update Shading.java

--- Shading.java
+++ Shading.java
@@ -1,9 +1,11 @@
 package setSolver;
 
+// Enum shading contains the possible color attributes for the card
 public enum Shading {
-	
+	// Shading
 	FULL, STRIPED, EMPTY;
 	
+	// Returns the code for each shading
 	public String getCode() {
 		if(this == FULL) {
 			return "F";
