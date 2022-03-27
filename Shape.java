8619c88440ad23d4fead3453a6399a4050f893ef
gworthin <102486685+gworthin@users.noreply.github.com>
2022-03-27 16:53:27 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,9 +1,11 @@
 package setSolver;
 
+// Enum shape contains the possible color attributes for the card
 public enum Shape{
-	
+	// Shapes
 	DIAMOND, SQUIGGLE, OVAL;
 	
+	// Returns the card code for each shape
 	public String getCode() {
 		if(this == DIAMOND) {
 			return "D";
@@ -13,6 +15,4 @@ public enum Shape{
 			return "O";
 		}
 	}
-	
-	
 }
